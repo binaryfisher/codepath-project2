aebdf4d109440d65a5a31978aa0230405dec5d26
yu900821 <ysw900821@gmail.com>
2023-02-28 00:18:07 -0500

    all function finished except for rotating card

---
+++ src/cardData.jsx
@@ -0,0 +1,5 @@
+
+
+const flashCards = [{question:"What is the DOM?", 
+  answer:"The Document Object Model (DOM) is the data representation of the objects that comprise the structure and content of a document on the web. "},
+{question:"What is Javascript used for?", answer:"JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else."}]
